fbe8b01dcc425109faa06cebb3a9bfc76f08a935
Ulysses Popple <deadmantennis@gmail.com>
2011-07-22 19:18:01 -0400

    Repo create for Android projects.

---
+++ SeeThatMovie/src/com/upopple/andoid/seethatmovie/SeeThatMovieActivity.java
@@ -0,0 +1,13 @@
+package com.upopple.andoid.seethatmovie;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SeeThatMovieActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
